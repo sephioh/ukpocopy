78c0185aac6f2b4c6c2e51294060b405c2b1390c
Abraao Alcantara <sephioh@gmail.com>
2018-05-07 00:37:49 -0300

    Enables installation with pip

---
+++ README.md
@@ -0,0 +1,14 @@
+ukpocopy
+========
+
+A python package for UK Postal Code validation and formatting.
+
+
+Installing
+----------
+
+Install using `pip`:
+
+```
+pip install -U ukpocopy
+```
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ukpocopy',
+      version='0.1',
+      description='A python package for UK Postal Code validation and formatting.',
+      url='http://github.com/sephioh/ukpocopy',
+      author='Abraão Alcântara',
+      author_email='sephioh@gmail.com',
+      license='MIT',
+      packages=[],
+      zip_safe=False)
