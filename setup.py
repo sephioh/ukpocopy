0f0677441befa9969134ee674427c94cad1ddcaf
Abraao Alcantara <sephioh@gmail.com>
2018-05-07 15:24:30 -0300

    Adds ukpocopy package

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='ukpocopy',
-      version='0.1',
+      version='0.1.0',
       description='A python package for UK Postal Code validation and formatting.',
+      long_description='A python package for UK Postal Code validation and formatting.',
       url='http://github.com/sephioh/ukpocopy',
       author='Abraão Alcântara',
       author_email='sephioh@gmail.com',
       license='MIT',
-      packages=[],
+      packages=['ukpocopy'],
       zip_safe=False)
