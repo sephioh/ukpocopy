9b78cc1fba7703a250f966aeaac968ea29d04950
Abraao Alcantara <sephioh@gmail.com>
2018-05-09 13:10:12 -0300

    Remove constants to make tests more explicit

--- ukpocopy/tests/test_validators.py
+++ ukpocopy/tests/test_validators.py
@@ -5,61 +5,45 @@ from ukpocopy.validators import validate_postcode, validate_single_digit_distric
     validate_second_position_letter, validate_third_position_letter, \
     validate_fourth_position_letter, validate_final_two_letters
 
-VALID_POSTCODE = "SW1W 0NY"
-VALID_POSTCODE_WITH_AA9A_9AA_FORMAT = "EC1A 1BB"
-VALID_POSTCODE_WITH_A9A_9AA_FORMAT = "W1A 0AX"
-VALID_POSTCODE_WITH_A9_9AA_FORMAT = "M1 1AE"
-VALID_POSTCODE_WITH_A99_9AA_FORMAT = "B33 8TH"
-VALID_POSTCODE_WITH_AA9_9AA_FORMAT = "CR0 6XH"
-VALID_POSTCODE_WITH_AA99_9AA_FORMAT = "DN55 1PT"
-VALID_POSTCODE_WITH_LOWER_CASE_LETTERS = "dn55 1pt"
-VALID_POSTCODE_WITH_MIXED_CASE_LETTERS = "dn55 1PT"
-
-INVALID_POSTCODE = "0000 000"
-INVALID_POSTCODE_AB1_1AA = "AB1 1AA"
-VALID_POSTCODE_WITHOUT_SPACE = "SW1W0NY"
-EMPTY_POSTCODE = ""
-
-
 class TestValidatePostcode(TestCase):
     def test_valid_postcode_returns_true(self):
-        self.assertTrue(validate_postcode(VALID_POSTCODE))
+        self.assertTrue(validate_postcode("SW1W 0NY"))
 
     def test_valid_postcode_with_AA9A_9AA_format_returns_true(self):
-        self.assertTrue(validate_postcode(VALID_POSTCODE_WITH_AA9A_9AA_FORMAT))
+        self.assertTrue(validate_postcode("EC1A 1BB"))
 
     def test_valid_postcode_with_A9A_9AA_format_returns_true(self):
-        self.assertTrue(validate_postcode(VALID_POSTCODE_WITH_A9A_9AA_FORMAT))
+        self.assertTrue(validate_postcode("W1A 0AX"))
 
     def test_valid_postcode_with_A9_9AA_format_returns_true(self):
-        self.assertTrue(validate_postcode(VALID_POSTCODE_WITH_A9_9AA_FORMAT))
+        self.assertTrue(validate_postcode("M1 1AE"))
 
     def test_valid_postcode_with_A99_9AA_format_returns_true(self):
-        self.assertTrue(validate_postcode(VALID_POSTCODE_WITH_A99_9AA_FORMAT))
+        self.assertTrue(validate_postcode("B33 8TH"))
 
     def test_valid_postcode_with_AA9_9AA_format_returns_true(self):
-        self.assertTrue(validate_postcode(VALID_POSTCODE_WITH_AA9_9AA_FORMAT))
+        self.assertTrue(validate_postcode("CR0 6XH"))
 
     def test_valid_postcode_with_AA99_9AA_format_returns_true(self):
-        self.assertTrue(validate_postcode(VALID_POSTCODE_WITH_AA99_9AA_FORMAT))
+        self.assertTrue(validate_postcode("DN55 1PT"))
 
     def test_valid_postcode_with_lower_case_letters_returns_true(self):
-        self.assertTrue(validate_postcode(VALID_POSTCODE_WITH_LOWER_CASE_LETTERS))
+        self.assertTrue(validate_postcode("dn55 1pt"))
 
     def test_valid_postcode_with_mixed_case_letters_returns_true(self):
-        self.assertTrue(validate_postcode(VALID_POSTCODE_WITH_MIXED_CASE_LETTERS))
+        self.assertTrue(validate_postcode("dn55 1PT"))
 
     def test_valid_postcode_without_space_returns_false(self):
-        self.assertFalse(validate_postcode(VALID_POSTCODE_WITHOUT_SPACE))
+        self.assertFalse(validate_postcode("SW1W0NY"))
 
     def test_invalid_postcode_returns_false(self):
-        self.assertFalse(validate_postcode(INVALID_POSTCODE))
+        self.assertFalse(validate_postcode("0000 000"))
 
     def test_invalid_postcode_AB1_1AA_returns_false(self):
-        self.assertFalse(validate_postcode(INVALID_POSTCODE_AB1_1AA))
+        self.assertFalse(validate_postcode("AB1 1AA"))
 
     def test_empty_postcode_returns_false(self):
-        self.assertFalse(validate_postcode(EMPTY_POSTCODE))
+        self.assertFalse(validate_postcode(""))
 
 
 class TestSingleDigitDistrictValidator(TestCase):
